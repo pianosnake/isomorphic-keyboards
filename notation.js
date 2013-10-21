3d2d6709c1529d776500c3c5c99c90b6323f6d14
Florin Alexandrescu <pianosnake@gmail.com>
2013-10-20 21:22:29 -0600

    Add sharps to the notation library

--- notation.js
+++ notation.js
@@ -17,13 +17,33 @@ function Notation(options){
 Notation.prototype = {
 	render: function(){
 		this.el.addClass("system");
-		this.el.append("<div class='clef'></div><div class='bars'></div><div class='whole-note'></div>");
+		this.el.append("<div class='clef'></div><div class='bars'></div><div class='modifier'></div><div class='whole-note'></div>");
 	},
 
 	show: function(note){
 		this.el.find(".bars").text(note);
-		var notePosition = this.positions[note.split("")[0]];
+		var noteProperties = note.split("");
+
+		var notePosition = this.positions[noteProperties[0]];
 		this.el.find(".whole-note").css("top", notePosition);
+
+		if(noteProperties[1] == "b"){
+			this.showFlat(notePosition);
+		}else{
+			this.hideModifier();
+		}
+	}, 
+
+	showFlat: function(position){
+		this.el.find(".modifier").addClass("flat").css("top", position-10);
+	},
+
+	showSharp: function(position){
+		this.el.find(".modifier").addClass("sharp").css("top", position-7);
+	},
+
+	hideModifier: function(){
+		this.el.find(".modifier").removeClass("sharp flat");
 	}
 
 
