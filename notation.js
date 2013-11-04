2eb4882c611a3b797413ff6eed68b40349cc096e
Florin Alexandrescu <pianosnake@gmail.com>
2013-11-03 20:10:11 -0700

    Make notation initialize from a div.

--- notation.js
+++ notation.js
@@ -1,32 +1,37 @@
-var svgns = "http://www.w3.org/2000/svg";
-var xlinkns = "http://www.w3.org/1999/xlink";
-
 function Notation(options){
-		
-	this.el = options.el[0].contentDocument;
-
-	this.note = this.el.getElementById("note");
-	this.sharp = this.el.getElementById("sharp");
-	this.flat = this.el.getElementById("flat");
-	this.eight = this.el.getElementById("eight");
-	this.staff = this.el.getElementById("staff");
-	this.staffBBox = this.staff.getBBox();
-
+	this.el = options.el;
+	this.svgns = "http://www.w3.org/2000/svg";
+	this.addSvgObj();
 	this.positions = {
-		"c": 0,
-		"d": -10,
-		"e": -20,
-		"f": -30,
-		"g": -40,
-		"a": -50,
-		"b": -60
+		"c": 0, "d": -10, "e": -20, "f": -30, "g": -40, "a": -50,"b": -60
+	};
+	this.octaveOffsets = {
+		3: 70, 4: 0, 5: -70, 6: -140
 	}
-
-	this.hideExtras();
-	this.hide(this.note);
 }
 
 Notation.prototype = {
+	addSvgObj: function(){
+		var self = this; 
+		var svgObj = document.createElement("object");
+		svgObj.data = "notation/note_on_staff.svg";
+		svgObj.type = "image/svg+xml";
+		svgObj.onload = function(){self.render()};
+		this.el.append(svgObj);
+	},
+
+	render: function(){
+		this.svg = this.el.find("object")[0].contentDocument;
+		this.note = this.svg.getElementById("note");
+		this.sharp = this.svg.getElementById("sharp");
+		this.flat = this.svg.getElementById("flat");
+		this.eight = this.svg.getElementById("eight");
+		this.staff = this.svg.getElementById("staff");
+		this.staffBBox = this.staff.getBBox();
+		this.hideExtras();
+		this.hide(this.note);
+	}, 
+
 	draw: function(note){
 		this.hideExtras();
 		this.show(this.note);
@@ -38,22 +43,14 @@ Notation.prototype = {
 			this.show(this.sharp, notePosition);
 		}
 
-		this.showHelpers(notePosition);
+		this.removeAllHelperLines();
+		this.showHelperLines(notePosition);
 
 		this.note.setAttribute("transform","translate(0,"+notePosition+")");
 	}, 
 
 	getPosition: function(note){
-		var start;
-		if(note.octave == 3){
-		    start  = 70
-		}else if(note.octave == 4){
-		    start  = 0
-		}else if (note.octave == 5){
-			start = -70
-		}else if (note.octave == 6){
-			start = -140
-		}
+		var start = this.octaveOffsets[note.octave];
 		return start + this.positions[note.text.charAt(0)];
 	}, 
 
@@ -72,34 +69,43 @@ Notation.prototype = {
 		this.hide(this.eight);
 	}, 
 
-	showHelpers: function(p){
+	removeAllHelperLines: function(){
 		while(this.staff.getElementsByClassName("helper").length > 0 ){
-			this.staff.removeChild(this.staff.getElementsByClassName("helper")[0])
-		}
+				this.staff.removeChild(this.staff.getElementsByClassName("helper")[0])
+			}
+	},
 
-		if(p>=0){
-			var lines = Math.floor(p/20) + 1 ;
+	addHelperLinesBelow: function(p){
+		var lines = Math.floor(p/20) + 1 ;
 			for(var i=0; i< lines; i++){
-				this.addHelper(this.staffBBox.height+this.staffBBox.y + (i * 20) + 18);
-			}
-		}else if(p<=-120){
-			var pMod = p * (-1) - 120;
-			var lines = Math.floor(pMod/20) + 1 ;
-			for(var i=0; i< lines; i++){
-				this.addHelper(this.staffBBox.y -((i+1)* 20));
-			}
+				this.addHelperLine(this.staffBBox.height+this.staffBBox.y + (i * 20) + 18);
 		}
 	}, 
 
-	addHelper: function(y){
-		var r = this.el.createElementNS(svgns, "rect");
+	addHelperLinesAbove: function(p){
+		var pMod = p * (-1) - 120;
+		var lines = Math.floor(pMod/20) + 1 ;
+		for(var i=0; i< lines; i++){
+			this.addHelperLine(this.staffBBox.y -((i+1)* 20));
+		}
+	},
+
+	showHelperLines: function(p){
+		if(p>=0){
+			this.addHelperLinesBelow(p);		
+		}else if(p<=-120){
+			this.addHelperLinesAbove(p);
+		}
+	}, 
+
+	addHelperLine: function(y){
+		var r = this.svg.createElementNS(this.svgns, "rect");
 		r.setAttributeNS(null, "height", 2);
 		r.setAttributeNS(null, "width", 45);
 		r.setAttributeNS(null, "class", "helper");
 		r.setAttributeNS(null, "x", 85);
 		r.setAttributeNS(null, "y", y);
-		this.el.getElementById("staff").appendChild(r);
+		this.staff.appendChild(r);
 	}
-
 
 }
